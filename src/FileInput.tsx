5bf385645d280891cfc7420b41dbc4e75f5d6d1c
Rodrigo <rodrigo.pizarro@creativedata.io>
2024-02-04 18:01:57 +0100

    cleanup

--- src/FileInput.tsx
+++ src/FileInput.tsx
@@ -1,7 +1,5 @@
 import React, { useRef } from "react";
 import { WebPDFLoader } from "langchain/document_loaders/web/pdf";
-import { getDocument } from "pdfjs-dist";
-// import { GlobalWorkerOptions } from "pdfjs-dist/build/pdf.worker.mjs";
 
 const FileInput = () => {
   const fileInputRef = useRef<HTMLInputElement>(null);
@@ -14,13 +12,6 @@ const FileInput = () => {
       // Read the PDF using pdfjs
       const arrayBuffer = await selectedFile.arrayBuffer();
       const pdfData = new Uint8Array(arrayBuffer);
-
-      // // Configure the worker source
-      // GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@4.0.379/build/pdf.worker.min.js`;
-
-      // Load the PDF
-      const loadingTask = getDocument({ data: pdfData });
-      console.log("PDF Loaded:", loadingTask);
 
       // Create a Blob from Uint8Array
       const blob = new Blob([pdfData], { type: "application/pdf" });
